c73366cdbc6866abf0dd49a11a9bd7830dd56acc
calebchoi33 <calebchoi3@gmail.com>
2021-02-23 11:46:23 -0800

    Update lines.c

--- lines.c
+++ lines.c
@@ -12,24 +12,26 @@ typedef struct {
 } point;
 
 typedef struct {
-    point p1;
-    point p2;
+    point point1;
+    point point2;
 } line;
 
 double distance(point a, point b) {
-    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    double distance;
+    distance = sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    return distance; 
 }
 
 double length(line a) {
-    return distance(a.p1, a.p2);
+    return distance(a.point1, a.point2);
 }
 
 double slope(line a) {
-    return (a.p1.y - a.p2.y)/(a.p1.x - a.p2.x);
+    return (a.point1.y - a.point2.y)/(a.point1.x - a.point2.x);
 }
 
 double yInt(line a) {
-    return a.p1.y - slope(a) * a.p1.x;
+    return a.point1.y - slope(a) * a.point1.x;
 }
 
 int linepointthing(point a, line b) {
@@ -37,11 +39,10 @@ int linepointthing(point a, line b) {
 }
 
 int main( int argc, char *argv[]) {
-    point a = {1, -1};
-    point b = {2, -2};
-    point c = {3, -3};
+    point a = {4, 2};
+    point b = {4, 1};
+    point c = {4, 0};
     line l = {a, b};
     printf("%d\n", linepointthing(c, l));
-
-    return; 
+    return 0; 
 }
